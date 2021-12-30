fa41be78e874b540244b2c4c272ce5886a88d6f7
Adrian <adriancaef@gmail.com>
2021-12-30 10:39:40 +0100

    create default form

---
+++ src/components/Form.vue
@@ -0,0 +1,102 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" @reset="onReset" v-if="show" class="m-5">
+      <b-form-group
+        id="input-group-1"
+        label="Your Name:"
+        label-for="input-1"
+        class="mb-3"
+      >
+        <b-form-input
+          id="input-1"
+          v-model="form.name"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-2"
+        label="Your phone number:"
+        label-for="input-2"
+        class="mb-3"
+      >
+        <b-form-input
+          id="input-2"
+          v-model="form.phone"
+          type="number"
+          placeholder="Enter phone number"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-3"
+        label="Email address:"
+        label-for="input-3"
+        class="mb-3"
+      >
+        <b-form-input
+          id="input-3"
+          v-model="form.email"
+          type="email"
+          placeholder="Enter email"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group
+        id="input-group-4"
+        label="Your birth date:"
+        label-for="input-4"
+        class="mb-3"
+      >
+        <b-form-input
+          id="input-4"
+          v-model="form.birth"
+          placeholder="Enter name"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-button type="submit" variant="primary">Submit</b-button>
+      <b-button type="reset" variant="danger">Reset</b-button>
+    </b-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Form",
+  data() {
+    return {
+      form: {
+        name: "",
+        email: "",
+        phone: null,
+        birth: "",
+      },
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      alert(JSON.stringify(this.form));
+    },
+    onReset(event) {
+      event.preventDefault();
+      this.form.email = "";
+      this.form.name = "";
+      this.form.food = null;
+      this.form.checked = [];
+      this.show = false;
+      this.$nextTick(() => {
+        this.show = true;
+      });
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/views/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <Form />
+</template>
+
+<script>
+// @ is an alias to /src
+import Form from "@/components/Form.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Form,
+  },
+};
+</script>
