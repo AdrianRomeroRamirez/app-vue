7e19de27091ccaed1e3f93c5c2a874d132420381
Adrian <adriancaef@gmail.com>
2021-12-30 12:57:08 +0100

    init search

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="home">
+    <Search />
     <Users />
   </div>
 </template>
@@ -7,11 +8,13 @@
 <script>
 // @ is an alias to /src
 import Users from "@/components/Users.vue";
+import Search from "@/components/Search.vue";
 
 export default {
   name: "Home",
   components: {
     Users,
+    Search,
   },
 };
 </script>
